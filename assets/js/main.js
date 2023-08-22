da8e2259e3d8c298b8fccd17a77601da13f67446
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-08-22 15:23:24 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/*
+Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato,
+che va formattato in questo modo: nomecognomecolorepreferito23 
+Esempio: fabiopacificiblue23
+*/
+
